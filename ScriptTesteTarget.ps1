068e7a16facfa311b940dccd05fa85c61560632c
David Nunes <33230309+davidnunes03@users.noreply.github.com>
2021-05-22 23:34:23 -0300

    Create ScriptTesteTarget.ps1

---
+++ ScriptTesteTarget.ps1
@@ -0,0 +1,6 @@
+#TESTE-FABRIKAM - FABRIKAN - Destination
+$FABRIKAM_Tenant = "TenantIdFabrikam"
+$appID = "appId" 
+
+#Login with the admin testeCONTOSOmail
+./VerifySetup.ps1 -PartnerTenantId $FABRIKAM_Tenant -ApplicationId $appID 
