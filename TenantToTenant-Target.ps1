d3234b51ca5e3a0c8b02c97deea54e23354c7b23
David Nunes <33230309+davidnunes03@users.noreply.github.com>
2021-05-22 21:46:41 -0300

    Update TenantToTenant-Target.ps1

--- TenantToTenant-Target.ps1
+++ TenantToTenant-Target.ps1
@@ -5,8 +5,8 @@ $CONTOSO_DOMAIN = "testeCONTOSOmail.onmicrosoft.com" #Exchange Domain
 $CONTOSO_ADMIN = admin@testeCONTOSOmail.onmicrosoft.com #Exchange Admin
 
 #TESTE-FABRIKAM - Target
-$FABRIKAM_Tenant = "313494ba-73c3-4a79-a1fc-5cf7237775ef"  #Insert the Tenant ID e.g: "be80bafc-dadd-4961-a2ac-13a65a1d4983"
-$FABRIKAM_Subscription = "3a5934a8-a001-4d46-b8e3-6b545a0091eb" #Insert the Tenant subscription e.g: "f54f54cc-0d8d-4557-9854-6a5bac1f6b9b"
+$FABRIKAM_Tenant = "tenantid"  #Insert the Tenant ID e.g: "313494ba-73c3-4a79-a1fc-5cf7237775ef"
+$FABRIKAM_Subscription = "subscriptionid" #Insert the Tenant subscription e.g: "3a5934a8-a001-4d46-b8e3-6b545a0091eb"
 $FABRIKAM_DOMAIN = "testeFABRIKAMmail.onmicrosoft.com" #Exchange Domain
 $FABRIKAM_ADMIN = admin@testeFABRIKAMmail.onmicrosoft.com #Exchange Admin
 
