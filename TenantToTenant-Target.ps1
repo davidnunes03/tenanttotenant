eeeb81888bfb5dd3f542911c11d6b11f265c462e
David Nunes <33230309+davidnunes03@users.noreply.github.com>
2021-05-22 21:38:33 -0300

    Update TenantToTenant-Target.ps1

--- TenantToTenant-Target.ps1
+++ TenantToTenant-Target.ps1
@@ -1,15 +1,14 @@
 #TESTE-CONTOSO - Source
-$CONTOSO_Tenant = "be80bafc-dadd-4961-a2ac-13a65a1d4983"
-$CONTOSO_Subscription = "f54f54cc-0d8d-4557-9854-6a5bac1f6b9b"
-$CONTOSO_DOMAIN = "testeCONTOSOmail.onmicrosoft.com"
-$CONTOSO_ADMIN = admin@testeCONTOSOmail.onmicrosoft.com
+$CONTOSO_Tenant = "tenantid" #Insert the Tenant ID e.g: "be80bafc-dadd-4961-a2ac-13a65a1d4983"
+$CONTOSO_Subscription = "subscriptionid" #Insert the Tenant subscription e.g: "f54f54cc-0d8d-4557-9854-6a5bac1f6b9b"
+$CONTOSO_DOMAIN = "testeCONTOSOmail.onmicrosoft.com" #Exchange Domain
+$CONTOSO_ADMIN = admin@testeCONTOSOmail.onmicrosoft.com #Exchange Admin
 
 #TESTE-FABRIKAM - Target
-$FABRIKAM_Tenant = "313494ba-73c3-4a79-a1fc-5cf7237775ef"
-$FABRIKAM_Subscription = "3a5934a8-a001-4d46-b8e3-6b545a0091eb"
-$FABRIKAM_DOMAIN = "testeFABRIKAMmail.onmicrosoft.com"
-
-$FABRIKAM_ADMIN = admin@testeFABRIKAMmail.onmicrosoft.com
+$FABRIKAM_Tenant = "313494ba-73c3-4a79-a1fc-5cf7237775ef"  #Insert the Tenant ID e.g: "be80bafc-dadd-4961-a2ac-13a65a1d4983"
+$FABRIKAM_Subscription = "3a5934a8-a001-4d46-b8e3-6b545a0091eb" #Insert the Tenant subscription e.g: "f54f54cc-0d8d-4557-9854-6a5bac1f6b9b"
+$FABRIKAM_DOMAIN = "testeFABRIKAMmail.onmicrosoft.com" #Exchange Domain
+$FABRIKAM_ADMIN = admin@testeFABRIKAMmail.onmicrosoft.com #Exchange Admin
 
 #Execute o Script com o admin testeFABRIKAMmail.onmicrosoft.com
 
